4d39493dd91328cb607eac08ee5bf56b9bfe0514
Наумов Антон рис 19-1б <66029262+kkkaktusss@users.noreply.github.com>
2022-01-24 05:10:12 +0500

    Работа с интерфейсом, добавлена конструкция try/catch для обработки исключения( отправка фото на почту)

--- FaceChecker/Form1.Designer.cs
+++ FaceChecker/Form1.Designer.cs
@@ -30,24 +30,22 @@
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.Camera = new System.Windows.Forms.PictureBox();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.button3 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.Cameras = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
             this.MakeFoto = new System.Windows.Forms.Button();
             this.StreamVideoButton = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Camera)).BeginInit();
             this.tableLayoutPanel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -77,6 +75,14 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Камера";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(441, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
             // Camera
             // 
             this.Camera.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -91,91 +97,57 @@
             // 
             this.tableLayoutPanel2.ColumnCount = 1;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Controls.Add(this.button3, 0, 7);
             this.tableLayoutPanel2.Controls.Add(this.pictureBox1, 0, 3);
-            this.tableLayoutPanel2.Controls.Add(this.textBox1, 0, 6);
-            this.tableLayoutPanel2.Controls.Add(this.groupBox2, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.button1, 0, 5);
             this.tableLayoutPanel2.Controls.Add(this.MakeFoto, 0, 2);
             this.tableLayoutPanel2.Controls.Add(this.StreamVideoButton, 0, 1);
             this.tableLayoutPanel2.Controls.Add(this.button2, 0, 4);
+            this.tableLayoutPanel2.Controls.Add(this.button3, 0, 8);
+            this.tableLayoutPanel2.Controls.Add(this.textBox1, 0, 7);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 6);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(656, 3);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.RowCount = 8;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18.90756F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.76471F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13.53846F));
+            this.tableLayoutPanel2.RowCount = 9;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.475936F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.85562F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 19.7861F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 55.69231F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 43F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 38F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 68F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 17F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
             this.tableLayoutPanel2.Size = new System.Drawing.Size(212, 533);
             this.tableLayoutPanel2.TabIndex = 2;
             // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(3, 495);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 28);
-            this.button3.TabIndex = 10;
-            this.button3.Text = "Send";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(3, 176);
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 140);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(206, 141);
+            this.pictureBox1.Size = new System.Drawing.Size(206, 169);
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(3, 467);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(206, 20);
-            this.textBox1.TabIndex = 6;
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.Cameras);
-            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox2.Location = new System.Drawing.Point(3, 3);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(206, 68);
-            this.groupBox2.TabIndex = 0;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Выбор камеры";
-            // 
-            // Cameras
-            // 
-            this.Cameras.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Cameras.FormattingEnabled = true;
-            this.Cameras.Location = new System.Drawing.Point(3, 16);
-            this.Cameras.Name = "Cameras";
-            this.Cameras.Size = new System.Drawing.Size(200, 21);
-            this.Cameras.TabIndex = 0;
-            this.Cameras.SelectedIndexChanged += new System.EventHandler(this.Cameras_SelectedIndexChanged);
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(3, 439);
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(3, 383);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 22);
+            this.button1.Size = new System.Drawing.Size(206, 54);
             this.button1.TabIndex = 8;
-            this.button1.Text = "Detect";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "Обнаружить";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // MakeFoto
             // 
             this.MakeFoto.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.MakeFoto.Location = new System.Drawing.Point(3, 123);
+            this.MakeFoto.Location = new System.Drawing.Point(3, 78);
             this.MakeFoto.Name = "MakeFoto";
-            this.MakeFoto.Size = new System.Drawing.Size(206, 47);
+            this.MakeFoto.Size = new System.Drawing.Size(206, 56);
             this.MakeFoto.TabIndex = 1;
             this.MakeFoto.Text = "Фото";
             this.MakeFoto.UseVisualStyleBackColor = true;
@@ -184,9 +156,9 @@
             // StreamVideoButton
             // 
             this.StreamVideoButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.StreamVideoButton.Location = new System.Drawing.Point(3, 77);
+            this.StreamVideoButton.Location = new System.Drawing.Point(3, 13);
             this.StreamVideoButton.Name = "StreamVideoButton";
-            this.StreamVideoButton.Size = new System.Drawing.Size(206, 40);
+            this.StreamVideoButton.Size = new System.Drawing.Size(206, 59);
             this.StreamVideoButton.TabIndex = 2;
             this.StreamVideoButton.Text = "Запуск";
             this.StreamVideoButton.UseVisualStyleBackColor = true;
@@ -194,22 +166,47 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(3, 396);
+            this.button2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(3, 315);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(206, 62);
             this.button2.TabIndex = 9;
-            this.button2.Text = "Save";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Text = "Сохранить";
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // label1
+            // button3
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(441, 3);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Адрес почты";
+            this.button3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Location = new System.Drawing.Point(3, 484);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(206, 46);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Отправить";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 460);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(206, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(3, 440);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(206, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Введите почту";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // Form1
             // 
@@ -219,7 +216,6 @@
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
@@ -227,7 +223,6 @@
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -238,8 +233,6 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.PictureBox Camera;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.ComboBox Cameras;
         private System.Windows.Forms.Button MakeFoto;
         private System.Windows.Forms.Button StreamVideoButton;
         private System.Windows.Forms.PictureBox pictureBox1;
@@ -248,6 +241,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- FaceChecker/Form1.cs
+++ FaceChecker/Form1.cs
@@ -17,8 +17,6 @@ namespace FaceChecker
 {
     public partial class Form1 : Form
     {
-        private DsDevice[] webCams = null;
-        private int selectedCamId = 0;
         public Capture capture = null;
         FaceRec faceRec = new FaceRec();
         public Form1()
@@ -31,83 +29,14 @@ namespace FaceChecker
             groupBox1.Text = "Камера";
             faceRec.openCamera(Camera, pictureBox1);
         }
-        #region Неиспользуемое
-        private void StreamVideo()
-        {
-            try
-            {
-                if (webCams.Length == 0 || Cameras.SelectedItem == null)
-                    throw new Exception("Нет камеры!");
-                else if (capture != null)
-                    capture.Start();
-                else
-                {
-                    capture = new Capture(selectedCamId);
-                    capture.ImageGrabbed += Capture_ImageGrabbed;
-                    capture.Start();
-                }
-
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show(e.Message);
-            }
-        }
-        public void StopVideoCapture()
-        {
-            try
-            {
-                if (capture != null)
-                {
-                    capture.Pause();
-                    capture.Dispose();
-                    capture = null;
-                    Camera.Image.Dispose();
-                    //pictureBox1.Image = null;
-
-                    selectedCamId = 0;
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-        public void Capture_ImageGrabbed(object sender, EventArgs e)
-        {
-            try
-            {
-                Mat mat = new Mat();
-                capture.Retrieve(mat);
-                Camera.Image = mat.ToImage<Bgr, byte>().Flip(Emgu.CV.CvEnum.FlipType.Horizontal).ToBitmap();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-
-        private void Cameras_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            selectedCamId = Cameras.SelectedIndex;
-        }
-        #endregion
+      
 
         private void MakeFoto_Click(object sender, EventArgs e)
         {
             Bitmap bmp1 = new Bitmap(Camera.Image);
             bmp1.Save("123.jpeg");
-            //StopVideoCapture();
-            //groupBox1.Text = "Фото с камеры";
-            Camera.Image = bmp1;
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            webCams = DsDevice.GetDevicesOfCat(FilterCategory.VideoInputDevice);
-            foreach (var cam in webCams)
-                Cameras.Items.Add(cam.Name);
-        }
 
         private void button2_Click(object sender, EventArgs e)//добавить лицо в распознаватель
         {
@@ -127,30 +56,49 @@ namespace FaceChecker
         }
         public void SendMessage(string mailAddress)
         {
-            MailAddress from = new MailAddress("roketa1337@gmail.com", "Никита");
-            MailAddress to = new MailAddress(mailAddress);
-            MailMessage m = new MailMessage(from, to);
-            // тема письма
-            m.Subject = "Фотография";
-            // письмо представляет код html
-            m.IsBodyHtml = true;
-            m.AlternateViews.Add(getEmbeddedImage(@"D:\repos\FaceChecker\FaceChecker\bin\Debug\123.jpeg"));
-            // адрес smtp-сервера и порт, с которого будем отправлять письмо
-            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
-            // логин и пароль
-            smtp.Credentials = new NetworkCredential("roketa1337@gmail.com", "mjavtykzbayomoag");
-            smtp.EnableSsl = true;
-            smtp.Send(m);
+            if (mailAddress != "")
+            {
+                try
+                {
+                    MailAddress from = new MailAddress("roketa1337@gmail.com", "Никита");
+                    MailAddress to = new MailAddress(mailAddress);
+                    MailMessage m = new MailMessage(from, to);
+                    // тема письма
+                    m.Subject = "Фотография";
+                    // письмо представляет код html
+                    m.IsBodyHtml = true;
+                    m.AlternateViews.Add(getEmbeddedImage("123.jpeg"));
+                    // адрес smtp-сервера и порт, с которого будем отправлять письмо
+                    SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                    // логин и пароль
+                    smtp.Credentials = new NetworkCredential("roketa1337@gmail.com", "mjavtykzbayomoag");
+                    smtp.EnableSsl = true;
+                    smtp.Send(m);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка: " + ex);
+                }
+            }
+            else MessageBox.Show("Почта не найдена");
         }
 
         private AlternateView getEmbeddedImage(String filePath)
         {
-            LinkedResource res = new LinkedResource(filePath);
-            res.ContentId = Guid.NewGuid().ToString();
-            string htmlBody = @"<img src='cid:" + res.ContentId + @"'/>";
-            AlternateView alternateView = AlternateView.CreateAlternateViewFromString(htmlBody, null, MediaTypeNames.Text.Html);
-            alternateView.LinkedResources.Add(res);
-            return alternateView;
+                LinkedResource res = new LinkedResource(filePath);
+                res.ContentId = Guid.NewGuid().ToString();
+                string htmlBody = @"<img src='cid:" + res.ContentId + @"'/>";
+                AlternateView alternateView = AlternateView.CreateAlternateViewFromString(htmlBody, null, MediaTypeNames.Text.Html);
+                try
+                {
+                    alternateView.LinkedResources.Add(res);
+                return alternateView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex);
+                return alternateView;
+            }
         }
     }
 }
